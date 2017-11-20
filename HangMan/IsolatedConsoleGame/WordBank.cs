ef5a8f62cc1116f951edbc76764f6f850ee50d5a
Anthony Pham <anthonytpham31@gmail.com>
2017-11-19 21:53:34 -0700

    File Name Changed and Apparently Xamarin Reference

---
+++ HangMan/IsolatedConsoleGame/WordBank.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace HangMan.IsolatedConsoleGame
+{
+    public class WordBank
+    {
+        private static List<string> GetHangManWords()
+        {
+            var words = new List<string>();
+            try
+            {
+
+                using (var streamReader = new StreamReader("./HangManWords.txt"))
+                {
+                    var streamedWords = StreamWords(streamReader);
+                    return streamedWords;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Unable To Read File.");
+                Console.WriteLine(e.Message);
+            }
+            return words;
+        }
+
+        private static List<string> StreamWords(TextReader streamReader)
+        {
+            var checkForWords = true;
+            var words = new List<string>();
+            while (checkForWords)
+            {
+                var line = streamReader.ReadLine();
+                if (line == null || !line.Any())
+                {
+                    checkForWords = false;
+                }
+                else
+                {
+                    words.Add(line);
+                }
+            }
+            return words;
+        }
+
+        public string GetWord()
+        {
+            var wordList = GetHangManWords();
+            var rnd = new Random();
+            var randomizer = wordList.ElementAt(rnd.Next(wordList.Count));
+            return randomizer;
+        }
+    }
+}
--- HangMan/Program.cs
+++ HangMan/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using HangMan.IsolatedConsoleGame;
 
 namespace HangMan
 {
